29a863fbdef1c2b7736da1d3cf1346ed03dcbe7d
kobi824 <kobianthony8@gmail.com>
2023-06-05 23:48:45 -0500

    create server func

--- main.go
+++ main.go
@@ -15,6 +15,20 @@ type Server struct {
 	cache map[int]*Book
 }
 
+func NewServer() *Server {
+	db := make(map[int]*Book)
+	for i, _ := range [4000]int{} {
+		db[i+1] = &Book{
+			ID:    i + 1,
+			Title: fmt.Sprintf("Book-%d", i+1),
+		}
+	}
+	return &Server{
+		db:    db,
+		cache: make(map[int]*Book),
+	}
+}
+
 func main() {
 	fmt.Println("If a book exists in the database use the cache to eliminate db calls")
 	fmt.Println("using test to validate program")
